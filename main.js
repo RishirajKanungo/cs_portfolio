10e1bf04bf13090f4b879aa2557e266fb745007a
Rishiraj Kanungo <rishiraj.kanungo@gmail.com>
2019-08-08 14:36:00 -0400

    js fix

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-import Typed from Typed.js
+//import typed js framework "typed.js"
+import Typed from 'typed.js';
 
+//set up var for typing name to do the typing animation
 var typing_name = new Typed('#text',{
-    strings: ['My naem', 'My name is <strong>Rishiraj Kanungo</strong>']
+    strings: ['My naem', 'My name is <strong>Rishiraj Kanungo</strong>'],
     typeSpeed: 0,
     backSpeed:  0,
     smartBackspace: true,
+    showCursor: true,
     loop: true
 });
