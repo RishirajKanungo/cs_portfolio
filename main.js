bf9d6920adf610ce8647b0c87f68041ead2d6f78
Rishiraj Kanungo <rishiraj.kanungo@gmail.com>
2019-08-08 13:58:47 -0400

    JS file, logos, animations

---
+++ main.js
@@ -0,0 +1,9 @@
+import Typed from Typed.js
+
+var typing_name = new Typed('#text',{
+    strings: ['My naem', 'My name is <strong>Rishiraj Kanungo</strong>']
+    typeSpeed: 0,
+    backSpeed:  0,
+    smartBackspace: true,
+    loop: true
+});
