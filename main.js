0a0d6ff417f674904da3bb6fae214e71821d9a86
Rishiraj Kanungo <rishiraj.kanungo@gmail.com>
2019-08-09 14:25:43 -0400

    classify buttons
    working on trying to make sure that the buttons in the nav bar scroll down to div id

--- main.js
+++ main.js
@@ -1,30 +1,7 @@
-/*
-AnimateCircle("javaskill", .7)
-
-function AnimateCircle(container_id, animatePercentage, useColor) {
-    var useColor = '#FC5B3F';
-
-    var element = document.getElementById(container_id);
-    var circle = new ProgressBar.Circle(element, {
-        color: useColor,
-        duration: 2000,
-        easing: 'bounce',
-        strokeWidth: 5,
-        // Set default step function for all animate calls
-        step: function (state, circle) {
-            circle.path.setAttribute('stroke', state.color);
-        }
-    });
-
-    circle.animate(animatePercentage, {
-        from: {
-            color: useColor
-        },
-        to: {
-            color: useColor
-        }
-    });
-}
-*/
+$("button").click(function() {
+    $('html,body').animate({
+        scrollTop: $("").offset().top},
+        'slow');
+});
 
 console.log("hello");
