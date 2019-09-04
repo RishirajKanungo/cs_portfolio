e37e1eb890e23e19d56a11beaa8604c6f74bf8b5
Rishiraj Kanungo <rishiraj.kanungo@gmail.com>
2019-09-04 03:10:50 -0400

    slick js implementation
    able to implement slick js into the technical skills. Need to fix the height aspect with hovering over images. tbd.

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-$(document).ready(function(){
 
-    
-})
+$(document).ready(function(){
+    $('.skill_item').slick({
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 0
+            }
+          }
+        ]
+      });
+});
